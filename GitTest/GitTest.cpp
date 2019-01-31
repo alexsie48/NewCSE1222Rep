9b7727ebbaa4141d8d8c9e62674d6fd65ace1d81
alexsie48 <alexsie48@gmail.com>
2019-01-31 01:41:03 -0500

    well... stuff happens

--- GitTest/GitTest.cpp
+++ GitTest/GitTest.cpp
@@ -20,3 +20,5 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > this is just some dumb code thats taking up
+
+//lol
